d607d3b50a92b9d89625bbb29236286fce13dffd
Cristina <161821581+cris93jpn@users.noreply.github.com>
2025-04-23 10:30:42 +0900

    Create arugo_01.java

---
+++ Arugo-02/arugo_01.java
@@ -0,0 +1,13 @@
+
+public class arugo_01 {
+	public static void main(String[] args) {
+		int at;
+		at = 4;
+		if (at < 5) {
+			System.out.println("Bの値は" + (at * 3));
+		} else {
+			System.out.println("Bの値は" + (at * 2));
+		}
+	}
+
+}
