4c92be9f7717d0ee7f4f35f0e18dc1a5394b89f9
Cristina <161821581+cris93jpn@users.noreply.github.com>
2025-04-23 10:29:23 +0900

    Create arugo01_01.java

---
+++ Arugo-01/arugo01_01.java
@@ -0,0 +1,12 @@
+public class arugo01_01 {
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 150;
+		int c = 200;
+		int sum = a + b + c;
+		System.out.println("合計は" + sum);
+		int avg = sum / 3;
+		System.out.println("平均は" + avg);
+	}
+
+}
